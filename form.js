969496f65fda97aeee1a3ebea7857b1994ce5bc2
Lupa Costa <luis.14costa@gmail.com>
2025-06-23 21:36:36 -0300

    subindo aula formulário

---
+++ form.js
@@ -0,0 +1,9 @@
+btn_enviar.addEventListener('click', function(event) {
+
+    let nome = document.getElementById('nome').value;
+    let idade = document.getElementById('idade').value;
+
+    console.log(nome);
+    console.log(idade);
+    
+});
